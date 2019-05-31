e75586ccc0a45769713196c022d57f66a809c293
Ana Pichardo <kayablissfilms@gmail.com>
2019-05-31 03:08:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
+def fizzbuzz()
+  if int % 3 == 0
+    "Fizz"
+  end
+  if int % 5 == 0 
+    "Buzz"
+  end
+  if int % 3 == 0 && int % 5 == 0 
+   puts "FizzBuzz"
+  end
+  if int != int % 3 == && int != int % 5 == 0
+    "nil"
+  end
+    
+end
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
