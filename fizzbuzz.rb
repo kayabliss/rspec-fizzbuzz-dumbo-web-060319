23a56bfdb50614f79d1b737db504fd32c18f1e50
Ana Pichardo <kayablissfilms@gmail.com>
2019-05-31 03:17:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,8 +7,7 @@ def fizzbuzz()
   end
   if int % 3 == 0 && int % 5 == 0 
    puts "FizzBuzz"
- end
-  if int != int % 3 == 0 && int != int % 5 == 0
+  elsif int != int % 3 == 0 && int != int % 5 == 0
     "nil"
   end
     
